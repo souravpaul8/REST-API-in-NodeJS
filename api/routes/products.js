9538cf4326dad3582f2d2a8921f253e2e2c2e707
SOURAV PAUL <souravpaul@protonmail.com>
2020-06-04 00:14:00 +0530

    Basic Setup of POST, GET, DELETE, PATCH requests.

---
+++ api/routes/products.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET Requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: 'Handling POST Requests to /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req,res,next) => {
+    const id = req.params.productId;
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req,res,next) => {
+    res.status(200).json({
+        message: 'Updated Product !'
+    });
+});
+
+router.delete('/:productId', (req,res,next) => {
+    res.status(200).json({
+        message: 'Deleted Product!'
+    });
+});
+
+
+module.exports = router;
